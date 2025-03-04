175cd9d951cfda56f07e77c4c0b2c09cc0aa7257
Paweł Nowak <hitec00@interia.pl>
2025-03-04 09:36:32 +0100

    fixes in js script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-let changeColorButton = document.querySelector(".js-changeColorButton");
-let mainHeaderText = document.querySelector(".js-mainHeaderText");
-let nextColor = document.querySelector(".js-nextColor");
+{
+    const onClickChangeMainHeader = () => {
+        const nextColor = document.querySelector(".js-nextColor");
+        const mainHeaderText = document.querySelector(".js-mainHeaderText");
+        mainHeaderText.classList.toggle("mainHeader__text--dark");
+        nextColor.innerText = mainHeaderText.classList.contains("mainHeader__text--dark") ? 'blue' : 'gray';
+    };
 
-changeColorButton.addEventListener("click", () => {
-    mainHeaderText.classList.toggle("mainHeader__text--dark");
-    nextColor.innerText = mainHeaderText.classList.contains("mainHeader__text--dark") ?  'blue' : 'gray'
-});
+    const init = () => {
+        const changeColorButton = document.querySelector(".js-changeColorButton");
+        changeColorButton.addEventListener("click", onClickChangeMainHeader);
+    };
+
+    init();
+}
